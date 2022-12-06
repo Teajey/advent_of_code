30975cc56a26cce978068d1931f86e4bac450519
Teajey <21069848+Teajey@users.noreply.github.com>
2022-12-06 13:58:14 -1000

    2022 4 part two

--- 2022/4/src/main.rs
+++ 2022/4/src/main.rs
@@ -5,10 +5,10 @@ type Result<T, E = String> = std::result::Result<T, E>;
 mod range {
     use super::{parse_string_pair, Result};
 
-    pub struct Range(u32, u32);
+    pub struct Range(i32, i32);
 
     impl Range {
-        pub fn try_new(start: u32, end: u32) -> Result<Self> {
+        pub fn try_new(start: i32, end: i32) -> Result<Self> {
             if end < start {
                 return Err(format!("end ({end}) is before start ({start})"));
             }
@@ -17,13 +17,15 @@ mod range {
 
         pub fn try_parse(string: &str) -> Result<Range> {
             let (start, end) = parse_string_pair(string, '-')?;
-            let start = start.parse::<u32>().map_err(|err| format!("{err}"))?;
-            let end = end.parse::<u32>().map_err(|err| format!("{err}"))?;
+            let start = start.parse().map_err(|err| format!("{err}"))?;
+            let end = end.parse().map_err(|err| format!("{err}"))?;
             Range::try_new(start, end)
         }
 
-        pub fn contains(&self, other: &Range) -> bool {
-            self.0 <= other.0 && other.1 <= self.1
+        pub fn overlaps(&self, other: &Range) -> bool {
+            let start = self.0.max(other.0);
+            let end = self.1.min(other.1);
+            start <= end
         }
     }
 }
@@ -60,7 +62,7 @@ fn main() -> Result<()> {
         .map(parse_elf_assignment_pair)
         .collect::<Result<Vec<_>>>()?
         .into_iter()
-        .filter(|(first, second)| first.contains(second) || second.contains(first))
+        .filter(|(first, second)| first.overlaps(second))
         .count();
 
     println!("{reconsiderable_assignments}");
@@ -75,29 +77,33 @@ mod test {
     #[allow(clippy::bool_assert_comparison)]
     #[test]
     fn range_overlap() -> Result<()> {
-        let outer = Range::try_new(2, 4)?;
-        let inner = Range::try_new(6, 8)?;
-        assert_eq!(false, outer.contains(&inner));
+        let first = Range::try_new(2, 4)?;
+        let second = Range::try_new(6, 8)?;
+        assert_eq!(false, first.overlaps(&second));
 
-        let outer = Range::try_new(2, 3)?;
-        let inner = Range::try_new(4, 5)?;
-        assert_eq!(false, outer.contains(&inner));
+        let first = Range::try_new(2, 3)?;
+        let second = Range::try_new(4, 5)?;
+        assert_eq!(false, first.overlaps(&second));
 
-        let outer = Range::try_new(5, 7)?;
-        let inner = Range::try_new(7, 9)?;
-        assert_eq!(false, outer.contains(&inner));
+        let first = Range::try_new(5, 7)?;
+        let second = Range::try_new(7, 9)?;
+        assert_eq!(true, first.overlaps(&second));
 
-        let outer = Range::try_new(2, 8)?;
-        let inner = Range::try_new(3, 7)?;
-        assert_eq!(true, outer.contains(&inner));
+        let first = Range::try_new(2, 8)?;
+        let second = Range::try_new(3, 7)?;
+        assert_eq!(true, first.overlaps(&second));
 
-        let outer = Range::try_new(4, 6)?;
-        let inner = Range::try_new(6, 6)?;
-        assert_eq!(true, outer.contains(&inner));
+        let first = Range::try_new(4, 6)?;
+        let second = Range::try_new(6, 6)?;
+        assert_eq!(true, first.overlaps(&second));
 
-        let outer = Range::try_new(2, 6)?;
-        let inner = Range::try_new(4, 8)?;
-        assert_eq!(false, outer.contains(&inner));
+        let first = Range::try_new(2, 6)?;
+        let second = Range::try_new(4, 8)?;
+        assert_eq!(true, first.overlaps(&second));
+
+        let first = Range::try_new(68, 79)?;
+        let second = Range::try_new(42, 67)?;
+        assert_eq!(false, first.overlaps(&second));
 
         Ok(())
     }
