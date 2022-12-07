583bf289bba52d874806c2c4c4e439c982138c0f
Teajey <21069848+Teajey@users.noreply.github.com>
2022-12-07 11:47:45 -1000

    2022 6 part two
    I cannot f**king believe this code worked the first time I ran it!! 😵

--- 2022/6/src/main.rs
+++ 2022/6/src/main.rs
@@ -2,6 +2,25 @@ use std::io::Read;
 
 type Result<T, E = String> = std::result::Result<T, E>;
 
+#[allow(clippy::never_loop)]
+fn find_head_of_unique_string_of_length(length: usize, data: &[u8]) -> Option<usize> {
+    for i in length..(data.len()) {
+        'chars: {
+            let chars = &data[(i - length)..i];
+            for j in 0..chars.len() - 1 {
+                for k in j + 1..chars.len() {
+                    if chars[j] == chars[k] {
+                        break 'chars;
+                    }
+                }
+            }
+            return Some(i);
+        }
+    }
+
+    None
+}
+
 fn main() -> Result<()> {
     let stdin = std::io::stdin();
     let mut stdin = stdin.lock();
@@ -12,18 +31,10 @@ fn main() -> Result<()> {
         .read_to_string(&mut data)
         .map_err(|err| format!("Couldn't read stdin: {err}"))?;
 
-    for (i, _) in data.chars().enumerate().skip(4) {
-        let four_chars = &data[(i - 4)..i].as_bytes();
-        if four_chars[0] != four_chars[1]
-            && four_chars[0] != four_chars[2]
-            && four_chars[0] != four_chars[3]
-            && four_chars[1] != four_chars[2]
-            && four_chars[1] != four_chars[3]
-            && four_chars[2] != four_chars[3]
-        {
-            println!("{i}");
-            break;
-        }
+    let data = data.as_bytes();
+
+    if let Some(head) = find_head_of_unique_string_of_length(14, data) {
+        println!("{head}");
     }
 
     Ok(())
