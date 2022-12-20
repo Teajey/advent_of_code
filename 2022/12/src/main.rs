26928f65900fe24485b939a383c4950ee7f737d9
Teajey <21069848+Teajey@users.noreply.github.com>
2022-12-20 20:54:36 +1300

    2022 12 part two
    Certified over-engineered

--- 2022/12/src/main.rs
+++ 2022/12/src/main.rs
@@ -1,13 +1,27 @@
-use std::{collections::VecDeque, fmt::Debug, ops::Neg};
+use std::{
+    collections::VecDeque,
+    fmt::{Debug, Display},
+    ops::Neg,
+};
 
 use common::*;
 
 type Array<T> = Box<[T]>;
 
 enum Square {
-    Elevation(u32),
+    Elevation(u8),
     Start,
     End,
+}
+
+impl Square {
+    fn elevation(&self) -> u8 {
+        match self {
+            Square::Elevation(elevation) => *elevation,
+            Square::Start => b'a' - 96,
+            Square::End => b'z' - 96,
+        }
+    }
 }
 
 type Coord = (usize, usize);
@@ -76,7 +90,7 @@ impl Debug for Matrix<bool> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         for row in self.grid.iter() {
             for cell in row.iter() {
-                write!(f, "{}", if *cell { "#" } else { "." })?;
+                write!(f, "{}", if *cell { "▓" } else { "░" })?;
             }
             writeln!(f)?;
         }
@@ -85,9 +99,59 @@ impl Debug for Matrix<bool> {
 }
 
 struct Map {
-    grid: Matrix<Square>,
+    matrix: Matrix<Square>,
     start: Coord,
-    end: Coord,
+}
+
+impl Display for Map {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for row in self.matrix.grid.iter() {
+            for cell in row.iter() {
+                let ch = match cell {
+                    Square::Elevation(elevation) => (elevation + 96) as char,
+                    Square::Start => 'S',
+                    Square::End => 'E',
+                };
+                write!(f, "{ch}")?;
+            }
+            writeln!(f)?;
+        }
+
+        Ok(())
+    }
+}
+
+impl Map {
+    fn invert(self) -> Self {
+        let mut start = self.start;
+        let inverted_map_matrix = self
+            .matrix
+            .grid
+            .iter()
+            .enumerate()
+            .map(|(y, row)| {
+                row.iter()
+                    .enumerate()
+                    .map(|(x, cell)| match cell {
+                        Square::Elevation(elevation) => {
+                            Square::Elevation(Square::End.elevation() - elevation + 1)
+                        }
+                        Square::Start => Square::End,
+                        Square::End => {
+                            start = (x, y);
+                            Square::Start
+                        }
+                    })
+                    .collect::<Array<_>>()
+            })
+            .collect::<Array<_>>();
+
+        Self {
+            matrix: Matrix::try_from(inverted_map_matrix)
+                .expect("Matrix must be inverted from a valid Matrix"),
+            start,
+        }
+    }
 }
 
 impl TryFrom<&str> for Map {
@@ -95,7 +159,7 @@ impl TryFrom<&str> for Map {
 
     fn try_from(value: &str) -> Result<Self, Self::Error> {
         let mut start = None;
-        let mut end = None;
+        let mut found_end = false;
 
         let grid = value
             .split('\n')
@@ -109,19 +173,22 @@ impl TryFrom<&str> for Map {
                             Square::Start
                         }
                         'E' => {
-                            end = Some((x, y));
+                            found_end = true;
                             Square::End
                         }
-                        c => Square::Elevation(c as u32 - 97),
+                        c => Square::Elevation(c as u8 - 96),
                     })
                     .collect::<Array<_>>()
             })
             .collect::<Array<_>>();
 
+        if !found_end {
+            return Err(e!("Map did not contain an end point"));
+        }
+
         Ok(Self {
-            grid: Matrix::try_from(grid)?,
+            matrix: Matrix::try_from(grid)?,
             start: start.ok_or_else(|| e!("Map did not contain a start point"))?,
-            end: end.ok_or_else(|| e!("Map did not contain an end point"))?,
         })
     }
 }
@@ -195,7 +262,7 @@ impl<'a> MapSearcher<'a> {
         let mut queue = VecDeque::new();
         queue.push_back(map.start);
 
-        let searched = Matrix::from_elem(None, map.grid.width, map.grid.height);
+        let searched = Matrix::from_elem(None, map.matrix.width, map.matrix.height);
 
         Self {
             queue,
@@ -204,17 +271,12 @@ impl<'a> MapSearcher<'a> {
         }
     }
 
-    fn survey(
-        &mut self,
-        position: Coord,
-        elevation: Option<u32>,
-        direction: Direction,
-    ) -> Result<()> {
+    fn survey(&mut self, position: Coord, elevation: u8, direction: Direction) -> Result<()> {
         let Some(target_coord) = direction.step_from(position) else {
             return Ok(());
         };
 
-        let Some(target) = self.map.grid.get(target_coord).ok() else {
+        let Some(target) = self.map.matrix.get(target_coord).ok() else {
             return Ok(());
         };
 
@@ -222,10 +284,8 @@ impl<'a> MapSearcher<'a> {
             return Ok(());
         }
 
-        if let (Square::Elevation(target_elevation), Some(elevation)) = (target, elevation) {
-            if *target_elevation > elevation + 1 {
-                return Ok(());
-            }
+        if target.elevation() > elevation + 1 {
+            return Ok(());
         }
 
         self.queue.push_back(target_coord);
@@ -236,12 +296,13 @@ impl<'a> MapSearcher<'a> {
 
     fn search_for_end(&mut self) -> Result<Coord> {
         while let Some(coord) = self.queue.pop_front() {
-            match self.map.grid.get(coord)? {
+            match self.map.matrix.get(coord)? {
                 Square::Start => {
                     for direction in Direction::all() {
-                        self.survey(coord, None, *direction)?;
+                        self.survey(coord, Square::Start.elevation(), *direction)?;
                     }
                 }
+                Square::Elevation(elevation) if *elevation == 26 => return Ok(coord),
                 Square::Elevation(elevation) => {
                     let directions = self
                         .searched
@@ -249,7 +310,7 @@ impl<'a> MapSearcher<'a> {
                         .ok_or_else(|| e!("Expected queued Square to have been surveyed"))?
                         .others();
                     for direction in directions {
-                        self.survey(coord, Some(*elevation), direction)?;
+                        self.survey(coord, *elevation, direction)?;
                     }
                 }
                 Square::End => return Ok(coord),
@@ -280,10 +341,10 @@ impl<'a> MapSearcher<'a> {
 impl Debug for Direction {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Up => write!(f, "^"),
-            Self::Down => write!(f, "v"),
-            Self::Left => write!(f, "<"),
-            Self::Right => write!(f, ">"),
+            Self::Up => write!(f, "↑"),
+            Self::Down => write!(f, "↓"),
+            Self::Left => write!(f, "←"),
+            Self::Right => write!(f, "→"),
         }
     }
 }
@@ -326,6 +387,7 @@ fn main() -> Result<()> {
     let input = get_input()?;
 
     let map = Map::try_from(input.as_str())?;
+    let map = map.invert();
 
     let mut searcher = MapSearcher::init(&map);
 
@@ -333,12 +395,13 @@ fn main() -> Result<()> {
 
     let shortest_path = searcher.get_shortest_path(end)?;
 
-    let mut path_visual = Matrix::from_elem(false, map.grid.width, map.grid.height);
+    let mut path_visual = Matrix::from_elem(false, map.matrix.width, map.matrix.height);
 
     for coord in coords_from_directions(end, &shortest_path) {
         *path_visual.get_mut(coord)? = true;
     }
 
+    println!("Map:\n{}", map);
     println!("Search map:\n{:?}", searcher);
     println!("Shortest path visual:\n{:?}", path_visual);
     println!("End: {:?}", end);
@@ -349,19 +412,4 @@ fn main() -> Result<()> {
     println!("Shortest path length: {:?}", shortest_path.len());
 
     Ok(())
-}
-
-#[cfg(test)]
-mod test {
-    use crate::Direction;
-
-    #[test]
-    fn directions() {
-        let direction = Direction::Up;
-        let directions = direction.others().collect::<Vec<_>>();
-        assert_eq!(
-            &directions,
-            &[Direction::Right, Direction::Down, Direction::Left]
-        );
-    }
 }
